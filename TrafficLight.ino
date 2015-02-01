c0d34209c4afdb5cabe0347e0ea4ca8a2b56c49b
Martin Hans <martinhansdk@gmail.com>
2015-02-01 19:30:29 +0100

    Move towards a programmable schedule.

--- TrafficLight.ino
+++ TrafficLight.ino
@@ -5,85 +5,115 @@ volatile int timer_overflow_count = 0;
 #define GREEN_PIN 2
 
 volatile int timer_interrupt = 0;
+boolean is_master = false;
 
 /**
-  The timing of a traffic light determines how the light_time is translated
-  into light patterns. A and B timing are defined such that cars coming from
-  A and B direction never have green light at the same time.
+  A LightPattern specifies a duration and which lamps are on for that duration.
+  A schedule is a series of LightPatterns that are repeated in a circular fashion.
+  
+  A patterns with duration 0 indicates no pattern and is used to mark the end of the schedule.
 */
-typedef enum {A, B} LightTiming;
-LightTiming timing = A;
+class LightPattern {
+public:
+  int duration; // unit: 1/2 seconds
+  boolean red;
+  boolean yellow;
+  boolean green;
+};
+
+const int MAX_PATTERNS = 8;
+  
+const LightPattern DUMMY_PATTERN = {0, true, true, true};
+
+class Schedule {
+public:
+  LightPattern patterns[MAX_PATTERNS];
+  
+  void setPattern(int index, int duration, boolean red, boolean yellow, boolean green) {
+     if(index < MAX_PATTERNS) {
+        patterns[index].duration = duration;
+        patterns[index].red = red;
+        patterns[index].yellow = yellow;
+        patterns[index].green = green;
+     } 
+  }
+  
+  int cycle_time() {
+    int sum=0;
+    
+    for(int i = 0 ; i < MAX_PATTERNS ; i++) {
+       if(patterns[i].duration == 0) {
+          break; 
+       }
+       sum += patterns[i].duration;
+    }
+    
+    return sum;
+  }
+  
+  const LightPattern* currentPattern(int time) {
+    int pattern_end_time = 0;
+    for(int i = 0 ; i < MAX_PATTERNS ; i++) {
+       if(patterns[i].duration == 0) {
+          break; 
+       }
+       
+       pattern_end_time += patterns[i].duration;
+       
+       if(time < pattern_end_time) {
+         return &patterns[i];  
+       }
+    }
+    
+    // nothing found
+    return &DUMMY_PATTERN;
+  }
+  
+};
 
 /**
   light_time contains the global time which is shared by all traffic lights
   The correct state of the lights is computed from this.
-  The unit is seconds.
+  The unit is 1/2 seconds.
 */
 int light_time = 0;
-
-/* define the schedule
-*/
-#define YELLOW_RED_DURATION 1*2
-#define GREEN_DURATION 18*2
-#define YELLOW_DURATION 4*2
-#define BOTH_RED_DURATION 1*2
-// the duration of the red signal must be just so long that the light pointing
-// in the other direction can cycle through yellow-green-yellow plus a
-// little margin where botgh are red
-#define RED_DURATION (YELLOW_RED_DURATION + GREEN_DURATION + YELLOW_DURATION + BOTH_RED_DURATION)
-
-// calculate the cycle time and the switch times based on this
-#define CYCLE_TIME  (RED_DURATION + YELLOW_RED_DURATION + GREEN_DURATION + YELLOW_DURATION)
-#define SWITCH_TO_YELLOW_RED_TIME (RED_DURATION)
-#define SWITCH_TO_GREEN_TIME (SWITCH_TO_YELLOW_RED_TIME + YELLOW_RED_DURATION)
-#define SWITCH_TO_YELLOW_TIME (SWITCH_TO_GREEN_TIME + GREEN_DURATION)
-
-// the B timing switches to yellow/red shortly after the A timing
-// has switched to red
-#define B_OFFSET (SWITCH_TO_YELLOW_RED_TIME - BOTH_RED_DURATION)
+int cycle_time = 1;
+Schedule schedule;
 
 void set_lights(int light_time) {
-  if(timing == B) {
-    light_time = (light_time + B_OFFSET) % CYCLE_TIME;
-  }
-
-
-  int red = 0;
-  int yellow = 0;
-  int green = 0;
-
-  if(light_time < SWITCH_TO_YELLOW_RED_TIME) {
-    red = 1;
-  } else if(light_time < SWITCH_TO_GREEN_TIME) {
-    red = 1;
-    yellow = 1;
-  } else if(light_time < SWITCH_TO_YELLOW_TIME) {
-    green = 1;
-  } else {
-    yellow = 1;
-  }
-
-  // all off
-  // PORTB &= ~((1 << RED_PIN) | (1 << YELLOW_PIN) | (1 << GREEN_PIN));
-  // turn only the right ones on
-  // PORTB |= (red << RED_PIN) | (yellow << YELLOW_PIN) | (green << GREEN_PIN);
+  const LightPattern* pattern = schedule.currentPattern(light_time);
   
-  digitalWrite(RED_PIN, red);
-  digitalWrite(YELLOW_PIN, yellow);
-  digitalWrite(GREEN_PIN, green);
+  digitalWrite(RED_PIN, pattern->red);
+  digitalWrite(YELLOW_PIN, pattern->yellow);
+  digitalWrite(GREEN_PIN, pattern->green);
 }
 
-
-ISR(TIM0_OVF_vect) {
-
+ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
+{
+  digitalWrite(13, digitalRead(13) ^ 1);   // toggle LED pin
+  timer_interrupt = 1;
 }
 
 void setup() {
-   // Set LED ports to output
-   //DDRB = 1<<RED_PIN | 1<<YELLOW_PIN | 1<<GREEN_PIN;
-   pinMode(RED_PIN, OUTPUT);
-   pinMode(YELLOW_PIN, OUTPUT);
-   pinMode(GREEN_PIN, OUTPUT);
+  // define schedule
+  int i = 0;
+  schedule.setPattern(i++, 1*2, true, true, false); // yellow/red
+  schedule.setPattern(i++, 18*2, false, false, true); // green
+  schedule.setPattern(i++, 4*2, false, true, false); // yellow
+  // the duration of the red signal must be just so long that the light pointing
+  // in the other direction can cycle through yellow-green-yellow plus a
+  // little margin where both are red
+  schedule.setPattern(i, 0, false, false, false); // temporary end marker
+  int red_duration = schedule.cycle_time() + 1*2;
+  schedule.setPattern(i++, red_duration, true, false, false); // red 
+  schedule.setPattern(i, 0, false, false, false); // end marker
+
+  cycle_time = schedule.cycle_time();
+
+  // Set LED ports to output
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(YELLOW_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
 
   // initialize timer1 
   noInterrupts();           // disable all interrupts
@@ -98,18 +128,11 @@ void setup() {
   interrupts();             // enable all interrupts
 }
 
-ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
-{
-  digitalWrite(13, digitalRead(13) ^ 1);   // toggle LED pin
-  timer_interrupt = 1;
-}
-
 void loop() {
   // wait until timer isr was run
   while(!timer_interrupt);
   timer_interrupt = 0;
 
-  light_time = (light_time+1) % CYCLE_TIME;
+  light_time = (light_time+1) % cycle_time;
   set_lights(light_time);
-  
 }
